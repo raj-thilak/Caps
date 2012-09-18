3df6cf04aa4691127bb8dad68750b2990bec5a85
Rajthilak Murugesan <rajthilak.murugesan@asu.edu>
2012-09-17 17:15:26 -0700

    Adding a main routine to which subroutines are created by refering to Class and  Objects

---
+++ Midterm/src/main_routine.java
@@ -0,0 +1,9 @@
+
+public class main_routine {
+
+	public static void main (String[] args){
+		
+		File_read readobject=new File_read();	//Create Object to Class File_read
+		readobject.fileread();	//Uses the object to execute the method 'fileread'
+	}
+}

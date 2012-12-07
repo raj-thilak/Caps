64dc87ed832dfbed28ec7fecf3f42c10cfd8a9b6
Rajthilak Murugesan <rajthilak.murugesan@asu.edu>
2012-12-06 22:22:00 -0700

    P calc Q calc Working after Tinney Ordering

--- Final_Power_Flow/src/main_routine.java
+++ Final_Power_Flow/src/main_routine.java
@@ -2,13 +2,16 @@
 public class main_routine {
 	
 	public static void main (String[] args)
+
 	{
 	Sub_Routines Element = new Sub_Routines();
 	Element.Create_Y_Bus();
+	Element.Tinney_Ordering();
 	Element.Create_LU();
 	Element.rowtocolumn();
 	Element.columntorow();
-	
+	Element.initialize_voltage();
+	Element.injection_flow();
 	}
 	
 
